1a1d47a195dd154b05f26e6b4dd73d6cbe16fd57
paulinekors <paulinekors@gmail.com>
2020-04-17 09:54:06 +0200

    spreading objects

--- src/playground/redux-expensify.js
+++ src/playground/redux-expensify.js
@@ -1,5 +1,5 @@
 import { createStore, combineReducers } from "redux";
-import { v1 as uuid} from "uuid";
+import { v1 as uuid } from "uuid";
 
 // ADD_EXPENSE
 const addExpense = ({
@@ -19,13 +19,24 @@ const addExpense = ({
 });
 
 // REMOVE_EXPENSE
-const removeExpense = ( { id } = {}) => ({
-    type: "REMOVE_EXPENSE",
-    id
+const removeExpense = ({ id } = {}) => ({
+  type: "REMOVE_EXPENSE",
+  id,
 });
 
-// EDIT__EXPENSE
+// EDIT_EXPENSE
+const editExpense = (id, updates) => ({
+  type: "EDIT_EXPENSE",
+  id,
+  updates,
+});
+
 // SET_TEXT_FILTER
+const setTextFilter = (text = '') => ({
+    type: 'SET_TEXT_FILTER',
+    text
+});
+
 // SORT_BY_DATE
 // SORT_BY_AMOUNT
 // SET_START_DATE
@@ -38,15 +49,24 @@ const expensesReducerDefaultState = [];
 
 const expensesReducer = (state = expensesReducerDefaultState, action) => {
   switch (action.type) {
-    case 'ADD_EXPENSE':
-        return [
-            ...state,
-            action.expense
-        ];
-        //state.concat(action.expense) //this returns a new array and doesn't change state - push would change the state
-    case 'REMOVE_EXPENSE':
-            return state.filter(({ id }) => id !== action.id);
-        default:
+    case "ADD_EXPENSE":
+      return [...state, action.expense];
+    // = same as above this returns a new array and doesn't change state - push would change the state
+    //state.concat(action.expense)
+    case "REMOVE_EXPENSE":
+      return state.filter(({ id }) => id !== action.id);
+    case "EDIT_EXPENSE":
+      return state.map((expense) => {
+          if (expense.id === action.id) {
+              return {
+                ...expense, //grab the existing expense and properties
+                ...action.updates //overwrite the ones that are passed in
+              };
+          } else {
+              return expense; //do nothing
+          }
+      });
+    default:
       return state;
   }
 };
@@ -63,6 +83,11 @@ const filtersReducerDefaultState = {
 
 const filtersReducer = (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
+      case "SET_TEXT_FILTER": //return a new object so do not filter on state
+          return {
+              ...state,
+              text: action.text
+          };
     default:
       return state;
   }
@@ -79,18 +104,30 @@ const store = createStore(
 
 // track changes
 store.subscribe(() => {
-    console.log(store.getState());
+  console.log(store.getState());
 });
 
 // This action is going to get dispatched to BOTH reducers: filters and expenses
 // But adding an expense doesn't do anything to filtersReducer so we do not need to add a case here
-const expenseOne = store.dispatch(addExpense({ description: 'Coffee', amount: 300 }));
+const expenseOne = store.dispatch(
+  addExpense({ description: "Coffee", amount: 300 })
+);
 console.log(expenseOne);
-const expenseTwo = store.dispatch(addExpense({ description: 'Rent', amount: 100 }));
+const expenseTwo = store.dispatch(
+  addExpense({ description: "Rent", amount: 100 })
+);
 console.log(expenseTwo);
 
-const removeExpenseOne = store.dispatch(removeExpense({ id: expenseOne.expense.id }));
+const removeExpenseOne = store.dispatch(
+  removeExpense({ id: expenseOne.expense.id })
+);
 console.log(removeExpenseOne);
+
+store.dispatch(editExpense(expenseTwo.expense.id, { amount: 500 }));
+
+store.dispatch(setTextFilter('rent'));
+store.dispatch(setTextFilter());
+
 
 // check to see if it works and get the default state > prints it once
 // console.log(store.getState());
@@ -112,3 +149,17 @@ const demoState = {
     endDate: undefined,
   },
 };
+
+// OBJECT SPREAD OPERATOR
+// we need to change the babel settings
+
+const user = {
+  name: "Jen",
+  age: 24,
+};
+
+console.log({
+  ...user,
+  location: "Philly",
+  age: 27,
+});
