ba6f17a2792e5ea625b3bd26f7ddc93b43484cb4
paulinekors <paulinekors@gmail.com>
2020-04-18 09:57:06 +0200

    set up expenseItem, expenseList and Provider, connect

--- src/app.js
+++ src/app.js
@@ -1,18 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import AppRouter from './routers/AppRouter';
-import configureStore from './store/configureStore';
-import { addExpense } from './actions/expenses';
-import { setTextFilter } from './actions/filters';
-import getVisibleExpenses from './selectors/expenses';
+import { Provider } from "react-redux";
+import AppRouter from "./routers/AppRouter";
+import configureStore from "./store/configureStore";
+import { addExpense } from "./actions/expenses";
+import { setTextFilter } from "./actions/filters";
+import getVisibleExpenses from "./selectors/expenses";
 import "normalize.css/normalize.css";
 import "./styles/styles.scss";
 
 const store = configureStore();
 
-store.dispatch(addExpense( { description: 'water bill' }));
-store.dispatch(addExpense( { description: 'gas bill' }));
-store.dispatch(setTextFilter('water'));
+store.dispatch(addExpense({ description: "water bill", amount: 4500 }));
+store.dispatch(addExpense({ description: "gas bill" }));
+store.dispatch(setTextFilter("water"));
+
+setTimeout(() => {
+  store.dispatch(setTextFilter("bill"));
+}, 3000);
 
 const state = store.getState();
 const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
@@ -20,4 +25,10 @@ console.log(visibleExpenses);
 
 console.log(store.getState());
 
-ReactDOM.render(<AppRouter />, document.getElementById("app"));
+const jsx = (
+  <Provider store={store}>
+    <AppRouter />
+  </Provider>
+);
+
+ReactDOM.render(jsx, document.getElementById("app"));
