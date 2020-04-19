4f12d503f02ce8db8cc7fafa9ce60c26d83657d9
paulinekors <paulinekors@gmail.com>
2020-04-19 09:32:38 +0200

    added option to delete item and added search box for filtering

--- src/components/ExpenseListItem.js
+++ src/components/ExpenseListItem.js
@@ -1,10 +1,17 @@
 import React from "react";
+import { connect } from 'react-redux';
+import { removeExpense } from '../actions/expenses';
 
-const ExpenseListItem = ({ description, amount, createdAt }) => (
+// data is not persisted yet, refreshing brings the deleted items back
+const ExpenseListItem = ({ dispatch, id, description, amount, createdAt }) => (
    <div>
        <h3>{description}</h3>
        <p>{amount} - {createdAt}</p>
+       <button onClick={() => {
+           dispatch(removeExpense({ id }));
+       }}>Remove</button>
    </div>
 );
 
-export default ExpenseListItem;
+// we dont need anything from the state so we do not need mapStateToProps
+export default connect()(ExpenseListItem);
